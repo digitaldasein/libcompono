aa3175e20f13fdcb02be3ca13c59c11938c60531
svbaelen <senne@svbaelen.me>
2023-01-13 21:15:03 +0100

    fix submod dd-code

--- modules/dd-code/src/index.ts
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Digital Dasein <https://digitaldasein.org/>
-// SPDX-FileCopyrightText: 2022 Gerben Peeters <gerben@digitaldasein.org>
-// SPDX-FileCopyrightText: 2022 Senne Van Baelen <senne@digitaldasein.org>
-//
-// SPDX-License-Identifier: MIT
-
-export { DdCode } from './DdCode.js';
